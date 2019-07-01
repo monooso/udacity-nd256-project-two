351fa7a4a511a64e000412c0600d48cfced68a2f
Stephen Lewis <code@stephenlewis.me>
2019-07-01 08:22:53 +0100

    Reimplement LRU stack as an OrderedDict

--- problem_1.py
+++ problem_1.py
@@ -1,9 +1,9 @@
-from collections import deque
+from collections import OrderedDict
 
 
 class LeastRecentlyUsedStack(object):
     def __init__(self):
-        self.items = deque()
+        self.items = OrderedDict()
 
     def is_empty(self):
         """Return a boolean indicating whether the stack is empty"""
@@ -11,14 +11,14 @@ class LeastRecentlyUsedStack(object):
 
     def pop(self):
         """Return the least recently used item"""
-        if self.is_empty():
-            return None
-        value = self.items.popleft()
-        return self.pop() if value in self.items else value
+        return None if self.is_empty() else self.items.popitem(last=False)[0]
 
     def push(self, value):
         """Add an item to the LRU stack"""
-        self.items.append(value)
+        if value in self.items:
+            self.items.pop(value)
+        self.items[value] = value
+        return self
 
 
 class LeastRecentlyUsedCache(object):
@@ -58,36 +58,42 @@ class LeastRecentlyUsedCache(object):
 # Tests for LRU stack
 # -------------------
 stack = LeastRecentlyUsedStack()
-
-assert stack.is_empty() is True
-
 stack.push('a')
 stack.push('b')
 stack.push('c')
-stack.push('b')
+stack.push('b')     # Repeat value. LRU order should now be ['a', 'c', 'b']
 
 assert stack.is_empty() is False
 assert stack.pop() == 'a'
 assert stack.pop() == 'c'
 assert stack.pop() == 'b'
+
+# An empty stack returns None
 assert stack.is_empty() is True
+assert stack.pop() is None
 
 # -------------------
 # Tests for LRU cache
 # -------------------
 cache = LeastRecentlyUsedCache(5)
 
+# Retrieve an item from the empty cache
+assert cache.get('nope') == -1
+
 cache.set('a', 1)
 cache.set('b', 2)
 cache.set('c', 3)
 cache.set('d', 4)
+cache.set('e', 5)
 
+# If an item exists in the cache, it is returned
 assert cache.get('b') == 2
 assert cache.get('a') == 1
+
+# A missing item returns -1
 assert cache.get('z') == -1
 
-cache.set('e', 5)
-cache.set('f', 6)   # Replaces 'c' in the cache
-
+# Adding an item to a full cache removes the least recently used item ('c')
+cache.set('f', 6)
 assert cache.get('f') == 6
 assert cache.get('c') == -1
